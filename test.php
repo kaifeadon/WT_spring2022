0c7be112c1bd40f048e9c3bb848298845c64bd19
kaifeadon <98738266+kaifeadon@users.noreply.github.com>
2022-02-17 15:07:10 +0600

    MyTest3

---
+++ test.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+ <titlle>
+     My Site
+ </title>
+</head>
+<body>
+    <h1> Hello </h1>
+    <form action="Control/results.php"method="post">
+        Username<input type="text" name="uname" >
+        Password<input type="password" name="password">
+        <input type="submit" name="Submit"> 
+    </form>
+</body>
+</html>
